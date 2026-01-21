f7c515a40f4ab34adb345b61d5e6bd64519b1543
Claudio Russo <claudio@dfinity.org>
2026-01-21 22:16:00 +0000

    test script

---
+++ test.sh
@@ -0,0 +1,5 @@
+dfx stop
+dfx start --background --clean
+dfx deploy
+#add Candid ui as controller so it can access query methods
+dfx canister update-settings viewer_backend --add-controller $(dfx canister id __Candid_UI)
