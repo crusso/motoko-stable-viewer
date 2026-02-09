601560170c22daf1abae4c82664c70f020c21a3f
Claudio Russo <claudio@dfinity.org>
2026-02-09 18:07:50 +0000

    don't add ui as controller

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ dfx stop
 dfx start --background --clean
 dfx deploy
 #add Candid ui as controller so it can access query methods
-dfx canister update-settings viewer_backend --add-controller $(dfx canister id __Candid_UI)
+#dfx canister update-settings viewer_backend --add-controller $(dfx canister id __Candid_UI)
